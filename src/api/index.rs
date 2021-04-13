1f99655208042314685e2228770a20a733622c7d
Nep <spica@starry.blue>
2021-04-13 19:37:36 +0900

    Make get_index public

--- src/api/index.rs
+++ src/api/index.rs
@@ -1,7 +1,7 @@
 use actix_web::{Responder, HttpResponse};
 
 #[actix_web::get("/")]
-async fn get_index() -> impl Responder {
+pub async fn get_index() -> impl Responder {
     HttpResponse::PermanentRedirect()
         .header("Location", "https://github.com/SlashNephy/annict-profile-card")
         .finish()
