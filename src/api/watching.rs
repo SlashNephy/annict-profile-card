61127f49d100791db05afab892a5fdc05554fb4c
Nep <spica@starry.blue>
2021-04-18 17:14:22 +0900

    Error handling on encode_image

--- src/api/watching.rs
+++ src/api/watching.rs
@@ -4,12 +4,13 @@ use actix_web::web::{Path, Query};
 use graphql_client::GraphQLQuery;
 use sailfish::TemplateOnce;
 use serde::Deserialize;
-use futures::future::try_join_all;
+use futures::future::join_all;
 use log::*;
 
 use super::common;
 use watching_query::*;
 use log::Level::Trace;
+use crate::api::watching::SortKey::SatisfactionRate;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -21,13 +22,25 @@ struct WatchingQuery;
 
 #[derive(Deserialize)]
 pub struct WatchingParameter {
-    #[serde(default = "default_sort")]
-    sort: String,
+    #[serde(default)]
+    sort: SortKey,
     #[serde(default)]
     expose_image_url: bool
 }
 
-fn default_sort() -> String { String::from("satisfaction") }
+#[derive(Deserialize, PartialEq)]
+enum SortKey {
+    #[serde(alias = "watcher")]
+    WatchersCount,
+    #[serde(alias = "satisfaction")]
+    SatisfactionRate
+}
+
+impl Default for SortKey {
+    fn default() -> Self {
+        SatisfactionRate
+    }
+}
 
 #[derive(TemplateOnce)]
 #[template(path = "watching.svg")]
@@ -49,47 +62,77 @@ pub async fn get_watching(Path(username): Path<String>, query: Query<WatchingPar
             field: WorkOrderField::WATCHERS_COUNT
         },
         seasons: vec![String::from(common::CURRENT_SEASON)]
-    }).await.map_err(|e| ErrorInternalServerError(e))?;
+    }).await.map_err(|e| {
+        ErrorInternalServerError(e)
+    })?;
     
     if log_enabled!(Trace) {
         trace!("Response: {:#?}", &data);
     }
 
+    // ユーザオブジェクト
     let user: WatchingQueryUser = data.user.unwrap();
-    
-    let mut avatar_uri = user.avatar_url.unwrap();
-    if !query.expose_image_url {
-        avatar_uri = common::encode_image(avatar_uri)
-            .await
-            .map_err(|e| ErrorInternalServerError(e))?;
-    }
 
+    // プロフィール画像
+    let original_avatar_url = user.avatar_url.unwrap();
+    let avatar_uri = match query.expose_image_url {
+        true => original_avatar_url,
+        false => {
+            // base64 エンコードする
+            match common::encode_image(original_avatar_url).await {
+                Ok(uri) => uri,
+                _ => String::from("data:image/png;base64,")
+            }
+        }
+    };
+
+    // 作品のベクトル
     let mut works: Vec<WatchingQueryUserWorksNodes> = user
         .works.unwrap()
         .nodes.unwrap()
         .into_iter()
         .filter_map(|x| x)
         .collect();
-    if query.sort == "satisfaction" {
+    // 満足度の降順でソート
+    if query.sort == SortKey::SatisfactionRate {
         works.sort_unstable_by(|x, y| {
-            let rate_x = x.satisfaction_rate.unwrap_or(0f64);
-            let rate_y = y.satisfaction_rate.unwrap_or(0f64);
+            let rate_x: f64 = x.satisfaction_rate.unwrap_or(0.0);
+            let rate_y: f64 = y.satisfaction_rate.unwrap_or(0.0);
             rate_y.partial_cmp(&rate_x).unwrap()
         });
     }
     
-    let mut image_uris: Vec<String> = (&works).into_iter()
+    // 作品のアイキャッチ画像のベクトル
+    let original_image_uris: Vec<String> = (&works).into_iter()
         .filter_map(|x| x.image.as_ref())
         .filter_map(|x| x.recommended_image_url.as_ref())
         .map(|x| x.to_owned())
         .take(3)
         .collect();
-    if !query.expose_image_url {
-        image_uris = try_join_all(
-            image_uris.into_iter()
-                .map(|x| common::encode_image(x))
-        ).await.map_err(|e| ErrorInternalServerError(e))?;
-    }
+    let image_uris = match query.expose_image_url {
+        true => original_image_uris,
+        false => {
+            // 並列に base64 エンコードする
+            let job = join_all(
+                original_image_uris
+                    .into_iter()
+                    .map(|x| {
+                        common::encode_image(x)
+                    })
+            );
+    
+            job.await
+                .into_iter()
+                .map(|x| {
+                    match x {
+                        Ok(uri) => uri,
+                        // 失敗したら空画像に差し替える
+                        Err(_) => String::from("data:image/png;base64,")
+                    }
+                })
+                .collect()
+        }
+    };
     
     let svg = WatchingSvgTemplate {
         name: user.name,
@@ -99,7 +142,9 @@ pub async fn get_watching(Path(username): Path<String>, query: Query<WatchingPar
         image_uris
     }
         .render_once()
-        .map_err(|e| ErrorInternalServerError(e))?;
+        .map_err(|e| {
+            ErrorInternalServerError(e)
+        })?;
 
     Ok(
         HttpResponse::Ok()
